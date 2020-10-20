57a57a53a7b514b0bc17d94de0ecce9c3bdb5d12
TheWolvesTech <thewolvestech@protonmail.com>
2020-10-20 15:26:35 -0400

    lookea Ryu a commit equisde.

---
+++ myturtle.py
@@ -0,0 +1,16 @@
+from turtle import *
+import turtle
+#make a square
+shape("turtle")
+color("blue")
+for i in range(10):
+   turtle.forward(100)
+   turtle.left(90)
+   turtle.forward(100)
+   turtle.left(90)
+   turtle.forward(100)
+   turtle.left(90)
+   turtle.forward(100)
+   turtle.left(90)
+   turtle.forward(100)
+   turtle.done()
